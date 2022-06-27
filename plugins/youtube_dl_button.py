dc167e27865bfb7703c861b05eba3f97735fc0ad
masoudtestdeploy <95133886+masoudtestdeploy@users.noreply.github.com>
2022-06-27 20:05:47 +0430

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -44,12 +44,11 @@ async def youtube_dl_call_back(bot, update):
         name = nLink(p_id)
         url = "{} | KN.{}".format(link,name)
         youtube_dl_url = url
-        custom_file_name = name
     except:
         youtube_dl_url = update.message.reply_to_message.text
-        custom_file_name = str(response_json.get("title"))[:50] + "_" + youtube_dl_format + "." + youtube_dl_ext
-    
-    
+        
+        
+    custom_file_name = str(response_json.get("title"))[:50] + "_" + youtube_dl_format + "." + youtube_dl_ext
     youtube_dl_username = None
     youtube_dl_password = None
     if "|" in youtube_dl_url:
