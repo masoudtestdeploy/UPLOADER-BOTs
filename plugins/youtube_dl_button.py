0483db819bd5faaafe54cf6aab705555a34a9e36
masoudtestdeploy <95133886+masoudtestdeploy@users.noreply.github.com>
2022-06-27 19:59:27 +0430

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -22,77 +22,7 @@ from plugins.custom_thumbnail import *
 from pyrogram.types import InputMediaPhoto
 from helper_funcs.display_progress import progress_for_pyrogram, humanbytes
 
-from seedr import SeedrAPI
-import re 
 
-seedr = SeedrAPI(email="masoudakhoondi1@gmail.com", password="12345678")
-def human_readable_size(size, decimal_places=3):
-    for unit in ['B','KiB','MiB','GiB','TiB']:
-        if size < 1024.0:
-            break
-        size /= 1024.0
-    return f"{size:.{decimal_places}f}{unit}"
-def Get_Folders_ID():
-    num = 0 
-    output = ''
-    Get_Drives = seedr.get_drive()
-    #foldercount = len(Get_Drives["folders"])
-    folders = Get_Drives["folders"]
-    for i in folders:
-        idd = folders[num]["id"]
-        name = folders[num]["name"]
-        size = human_readable_size(folders[num]["size"])
-        out = "نام فایل :\n{} \nدانلود فولدر : /get_{} \nحجم فولدر : {} \n\n++++++++++++++\n\n".format(name,idd,size)
-        num = num+1
-        output = str(out + output)
-    space_used = human_readable_size(Get_Drives["space_used"])
-    space_max = human_readable_size(Get_Drives["space_max"])
-    siz = "کل فضای شما : {} \nفضای مصرفی شما : {}".format(space_max,space_used)
-    output = str(output + siz)
-    return output
-def Get_Files_ID(ID):
-    num = 0 
-    output = []
-    Get_Folder = seedr.get_folder(ID)
-    #foldercount = len(Get_Drives["folders"])
-    filesss = Get_Folder["files"]
-
-    for i in filesss:
-        idd = filesss[num]["folder_file_id"]
-        name = filesss[num]["name"]
-        sizz = human_readable_size(filesss[num]["size"])
-        out = {"id" : idd , "name" : name , "size" : sizz}
-        num = num+1
-        output.append(out)
-       
-    return output
-def Get_Link(ID):
-    Get_File_link = seedr.get_file(ID)["url"]
-    return Get_File_link
-def resualt_text(ID_File):
-    All = Get_Files_ID(ID_File)
-    num = 0 
-    output = ''
-    for i in All:
-        name = All[num]["name"]
-        ids =  All[num]["id"]
-        size =  All[num]["size"]
-        Link = Get_Link(ids)
-        text = "نام فایل : {} \nلینک دانلود : \n{}\nحجم فایل : {}\nدانلود فایل : /download_{}\n\n++++++++++++++\n\n".format(name,Link,size,ids)
-        num = num+1
-        output = output + str(text)
-    return output
-def Add_TR(Magnet):
-    output = seedr.add_torrent(Magnet)
-    if output["result"] == 'not_enough_space_added_to_wishlist':
-        title = output['wt']["title"]
-        res = {'stat':'not_enough_space_added_to_wishlist',"name":title} 
-    elif output["code"] == 200 :
-        title = output["title"]
-        res = {'stat':'ok',"name": title}
-    else :
-        res = {'stat':'no',"name": ""}
-    return res
 async def youtube_dl_call_back(bot, update):
     cb_data = update.data
     tg_send_type, youtube_dl_format, youtube_dl_ext = cb_data.split("|")
@@ -103,21 +33,8 @@ async def youtube_dl_call_back(bot, update):
     except (FileNotFoundError) as e:
         await update.message.delete(True)
         return False
-    
-    try:
-        pattern_link = re.compile(r'^\/download_(.*)')
-        matches_link = pattern_link.search(str(update.message.reply_to_message.text))
-        p_id = matches_link.group(1)
-        link = Get_Link(p_id)
-        name = seedr.get_file(p_id)["name"]
-        url = "{} | KN.{}".format(link,name)
-        youtube_dl_url = url
-    except:
-        #print("dasdhasdujhasiduhasiduaysidosahdoisdsudsadhsa")
-        youtube_dl_url = update.message.reply_to_message.text
-    
-    custom_file_name = os.path.basename(youtube_dl_url)
-    #custom_file_name = "KN"+name
+    youtube_dl_url = update.message.reply_to_message.text
+    custom_file_name = str(response_json.get("title"))[:50] + "_" + youtube_dl_format + "." + youtube_dl_ext
     youtube_dl_username = None
     youtube_dl_password = None
     if "|" in youtube_dl_url:
@@ -131,9 +48,9 @@ async def youtube_dl_call_back(bot, update):
             youtube_dl_username = url_parts[2]
             youtube_dl_password = url_parts[3]
         else:
-            for entity in youtube_dl_url:
+            for entity in update.message.reply_to_message.entities:
                 if entity.type == "text_link":
-                    youtube_dl_url = entity.youtube_dl_url
+                    youtube_dl_url = entity.url
                 elif entity.type == "url":
                     o = entity.offset
                     l = entity.length
@@ -147,20 +64,18 @@ async def youtube_dl_call_back(bot, update):
         if youtube_dl_password is not None:
             youtube_dl_password = youtube_dl_password.strip()
     else:
-        for entity in youtube_dl_url:
+        for entity in update.message.reply_to_message.entities:
             if entity.type == "text_link":
-                youtube_dl_url = entity.youtube_dl_url
+                youtube_dl_url = entity.url
             elif entity.type == "url":
                 o = entity.offset
                 l = entity.length
                 youtube_dl_url = youtube_dl_url[o:o + l]
     await bot.edit_message_text(
-    text=custom_file_name+"testtttttt",
+    text=Translation.DOWNLOAD_START,
     chat_id=update.message.chat.id,
     message_id=update.message.message_id)
-    print(234)
     description = Translation.CUSTOM_CAPTION_UL_FILE
-    print(567)
     if "fulltitle" in response_json:
         description = response_json["fulltitle"][0:1021]
         # escape Markdown and special characters
@@ -246,9 +161,8 @@ async def youtube_dl_call_back(bot, update):
             text=Translation.RCHD_TG_API_LIMIT.format(time_taken_for_download, humanbytes(file_size)),
             message_id=update.message.message_id)
         else:
-            
             await bot.edit_message_text(
-            text="test"+url,
+            text=Translation.UPLOAD_START,
             chat_id=update.message.chat.id,
             message_id=update.message.message_id)
             start_time = time.time()
@@ -320,8 +234,7 @@ async def youtube_dl_call_back(bot, update):
                     progress=progress_for_pyrogram,
                     progress_args=(Translation.UPLOAD_START,
                     update.message, start_time) )
-           
-                    
+
             asyncio.create_task(clendir(tmp_directory_for_each_user))
             asyncio.create_task(clendir(thumbnail))
             await bot.edit_message_text(
